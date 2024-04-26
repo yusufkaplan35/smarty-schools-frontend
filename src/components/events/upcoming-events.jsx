1cdf9f411e5d2d1b9f6a2a8955a139a9b57f662b
Scott <yusufs3435@gmail.com>
2024-04-26 20:32:12 +0200

    v11

--- src/components/events/upcoming-events.jsx
+++ src/components/events/upcoming-events.jsx
@@ -2,12 +2,39 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import SectionTitle from "../common/section-title";
+import events from "@/helpers/data/events.json";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import "./upcoming-events.scss";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const upcomingEvents = events.filter(
+	(item) => new Date(item.date) >= new Date()
+);
 
 const UpcomingEvents = () => {
 	return (
 		<div className="upcoming-events">
 			<Container>
-				<SectionTitle>Upcoming Events</SectionTitle>
+				<div className="title">
+					<span>
+						<FaChevronLeft />
+					</span>
+					<SectionTitle>Upcoming Events</SectionTitle>
+					<span>
+						<FaChevronRight />
+					</span>
+				</div>
+
+				<Swiper
+					spaceBetween={50}
+					slidesPerView={3}
+					onSlideChange={() => console.log("slide change")}
+					onSwiper={(swiper) => console.log(swiper)}
+				>
+					{upcomingEvents.map((item) => (
+						<SwiperSlide key={item.id}>Slide 1</SwiperSlide>
+					))}
+				</Swiper>
 			</Container>
 		</div>
 	);
