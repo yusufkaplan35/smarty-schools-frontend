a9dffb7a4c1de5272e6d4fc9ade0a822f6d852db
Scott <yusufs3435@gmail.com>
2024-04-26 21:33:03 +0200

    v15

---
+++ src/app/courses/page.jsx
@@ -0,0 +1,12 @@
+import PageHeader from '@/components/common/page-header'
+import React from 'react'
+
+const CoursesPage = () => {
+  return (
+    <>
+        <PageHeader>Courses</PageHeader>
+    </>
+  )
+}
+
+export default CoursesPage
