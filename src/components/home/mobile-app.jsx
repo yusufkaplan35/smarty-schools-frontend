a28af22957e330bae6e8cb793cfd9327e9b7296f
Scott <yusufs3435@gmail.com>
2024-04-26 21:17:09 +0200

    v14

--- src/components/home/mobile-app.jsx
+++ src/components/home/mobile-app.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { TfiAndroid, TfiApple } from "react-icons/tfi"
+import "./mobile-app.scss";
 
 const MobileApp = () => {
 	return (
 		<Container className="mobile-app">
-			<Row className="g-3">
-				<Col lg={7}>
+			<Row className="g-3 text-center align-items-center">
+				<Col lg={7} className="text-lg-start">
 					<h2>Are you ready to start your online course</h2>
 				</Col>
-				<Col lg={3}>
-					<a href="#" className="btn btn-outline-secondary">
+				<Col lg={5} className="text-lg-end">
+					<a href="#" className="btn btn-outline-secondary me-2">
 						<TfiApple/> Apple Store
 					</a>
 
