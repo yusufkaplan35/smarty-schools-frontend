2a3cace261ee7ae7285f8b47fc5ca86ca6715ade
Scott <yusufs3435@gmail.com>
2024-04-26 20:15:09 +0200

    v10

--- src/components/common/header/main-menu.jsx
+++ src/components/common/header/main-menu.jsx
@@ -1,15 +1,16 @@
-"use client"
+"use client";
 import React from "react";
 import { Nav } from "react-bootstrap";
 import menuItems from "@/helpers/data/main-menu.json";
 import Link from "next/link";
+import Icon from "../icon";
 
 const MainMenu = (props) => {
 	return (
 		<Nav {...props}>
 			{menuItems.map((item) => (
 				<Nav.Link href={item.link} key={item.link} as={Link}>
-					{item.title}
+					<Icon family="ai" icon={item.icon} className="text-primary" /> {item.title}
 				</Nav.Link>
 			))}
 		</Nav>
---
+++ src/components/common/icon.jsx
@@ -0,0 +1,23 @@
+import dynamic from "next/dynamic";
+import React from "react";
+
+const Icon = ({ family, icon, ...rest }) => {
+	const icons = {
+		ai: dynamic(() => import("react-icons/ai").then((item) => item[icon])),
+		tfi: dynamic(() =>
+			import("react-icons/tfi").then((item) => item[icon])
+		),
+		fa: dynamic(() => import("react-icons/fa").then((item) => item[icon])),
+		bi: dynamic(() => import("react-icons/bi").then((item) => item[icon])),
+		md: dynamic(() => import("react-icons/md").then((item) => item[icon])),
+		fi: dynamic(() => import("react-icons/fi").then((item) => item[icon])),
+	};
+
+	const IconPlaceHolder = family && icon ? icons[family] : null;
+
+	if (!IconPlaceHolder) return <></>;
+
+	return <IconPlaceHolder {...rest} />;
+};
+
+export default Icon;
